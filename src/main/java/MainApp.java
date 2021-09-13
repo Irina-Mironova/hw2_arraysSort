c5307319bfe160424d58de4becd81a5d4cf9d945
IrinaMironova <irina250282@mail.ru>
2021-09-13 14:57:52 +0500

    hw2

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,5 +1,43 @@
+import java.util.Random;
+
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("");
+        int n = 100000;
+        Random random = new Random();
+        MyArrayList<Integer> mal = new MyArrayList<>(n);
+
+        for (int i = 0; i < n; i++) {
+            mal.add(random.nextInt(100));
+        }
+        long start_time = System.currentTimeMillis();
+        mal.bubbleSort();
+        System.out.println("BubbleSort:" + (System.currentTimeMillis() - start_time) + "ms");
+        mal.clear();
+
+
+        for (int i = 0; i < n; i++) {
+            mal.add(random.nextInt(100));
+        }
+        start_time = System.currentTimeMillis();
+        mal.bubbleSortOptimized();
+        System.out.println("BubbleSortOptimized:" + (System.currentTimeMillis() - start_time) + "ms");
+        mal.clear();
+
+
+        for (int i = 0; i < n; i++) {
+            mal.add(random.nextInt(100));
+        }
+        start_time = System.currentTimeMillis();
+        mal.insertionSort();
+        System.out.println("InsertionSort:" + (System.currentTimeMillis() - start_time) + "ms");
+        mal.clear();
+
+
+        for (int i = 0; i < n; i++) {
+            mal.add(random.nextInt(100));
+        }
+        start_time = System.currentTimeMillis();
+        mal.selectionSort();
+        System.out.println("SelectionSort:" + (System.currentTimeMillis() - start_time) + "ms");
     }
 }
